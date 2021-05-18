3bc3bfecfa382466cdd143ea7e0cc754e49bc9c0
irostub <irostub@gmail.com>
2021-05-18 23:58:21 +0900

    Add exercise 2-1 to 2-8

---
+++ ch02/src/Ex2_6.java
@@ -0,0 +1,13 @@
+public class Ex2_6 {
+    public static void main(String[] args) {
+        int x = 100;
+        double pi = 3.14;
+        char ch = 'a';
+        String str = "abc";
+
+        System.out.println("x = " + x);
+        System.out.println("pi = " + pi);
+        System.out.println("ch = " + ch);
+        System.out.println("str = " + str);
+    }
+}
