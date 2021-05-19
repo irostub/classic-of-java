4a16fc64c7808c2067b2bca9e800c94675b09892
irostub <irostub@gmail.com>
2021-05-19 22:49:13 +0900

    Add exercise 2-9 to 2-12 and practice 2-1

---
+++ ch02/src/Px2_1.java
@@ -0,0 +1,9 @@
+public class Px2_1 {
+    public static void main(String[] args) {
+        System.out.println("          1byte     2byte     4byte     8byte  ");
+        System.out.println("논리형      bool                                 ");
+        System.out.println("문자형                char                       ");
+        System.out.println("정수형      byte      short     int       long   ");
+        System.out.println("실수형                          float     double ");
+    }
+}
